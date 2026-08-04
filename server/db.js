b1a1924f35be4704a2de41700a7743961109b74c
GitDaema <bedelj3@gmail.com>
2026-08-04 22:34:06 +0900

    Cloud: 클라우드 배포 파이프라인 및 DB 연동 환경 구축
    Vercel 호스팅 라우팅 및 Aiven MySQL SSL 커넥션 설정

--- server/db.js
+++ server/db.js
@@ -15,6 +15,9 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+
+  // [Cloud] 로컬 개발 접속 거부 방지를 위해 Vercel 배포 환경 전용 SSL 설정 적용
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined
 });
 
 module.exports = pool;
