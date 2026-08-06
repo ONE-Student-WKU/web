53ed91173fb92b32ccbc6c90d7fc3ede4dc87b98
GitDaema <bedelj3@gmail.com>
2026-08-06 14:32:02 +0900

    Cloud: Aiven DB SSL CA 인증서 검증 옵션 적용

--- server/db.js
+++ server/db.js
@@ -16,8 +16,8 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
 
-  // [Cloud] 로컬 개발 접속 거부 방지를 위해 Vercel 배포 환경 전용 SSL 설정 적용
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined
+  // [Cloud] 로컬 개발 접속 거부 방지를 위해 Vercel 배포 환경 전용 SSL 설정 및 CA 인증서 적용
+  ssl: process.env.DB_SSL === 'true' ? { ca: process.env.DB_CA, rejectUnauthorized: true } : undefined
 });
 
 module.exports = pool;
